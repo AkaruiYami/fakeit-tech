ad8dacf7bfb32dbfb6ee699fdc429d46dfc9784d
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2026-01-26 15:51:43 +0800

    add ai fakeit

--- src/modules/ai.rs
+++ src/modules/ai.rs
@@ -1,9 +1,12 @@
-#![allow(dead_code)]
+use std::thread;
+use std::time::Duration;
+
 use colored::*;
 use rand::Rng;
 use rand::rngs::ThreadRng;
 
 use crate::engine::FakeModule;
+use crate::modules::registry;
 
 pub struct AiModule;
 
@@ -12,9 +15,17 @@ impl FakeModule for AiModule {
         "ai"
     }
 
+    // TODO: Let the percentage properly sequential instead of jumble random
     fn run(&self, rng: &mut ThreadRng) {
         let percent = rng.random_range(1..=100);
 
         println!("{}", format!("[AI] Training model: {}%", percent).blue());
+        let delay_ms = rng.random_range(50..=350);
+        thread::sleep(Duration::from_millis(delay_ms));
     }
+}
+
+#[ctor::ctor]
+fn register_build() {
+    registry::register(Box::new(AiModule));
 }
